3c9ed67c907116a4c7005740e7e6094589f2ba26
rscheiwe <rscheiwe@gmail.com>
2018-09-21 17:49:12 -0400

    updated new elements, about, footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Footer from './components/Footer.js'
+import About from './components/About.js'
+
 import './App.css';
 
 class App extends Component {
@@ -12,20 +15,22 @@ class App extends Component {
       <div id="Stage">
         <div className="Container">
 
-<h3 style={{textAlign:'center'}}>Richard Scheiwe</h3>
+        <h3 style={{textAlign:'center'}}>Richard Scheiwe</h3>
 
           <div className='parent flex-parent'>
-            <div id="zoom" className='child1 flex-child' onClick={this.handleClick}>A</div>
-            <div id="zoom" className='child2 flex-child'>B</div>
-            <div id="zoom" className='child3 flex-child'>C</div>
-            <div id="zoom" className='child4 flex-child'>D</div>
-            <div id="zoom" className='child5 flex-child'>E</div>
-            <div id="zoom" className='child6 flex-child'>F</div>
-            <div id="zoom" className='child7 flex-child'>G</div>
-            <div id="zoom" className='child8 flex-child'>H</div>
+            <div id="zoom" className='child1 flex-child' onClick={this.handleClick}><div className="inner">A</div></div>
+            <div id="zoom" className='child2 flex-child'><div className="inner">B</div></div>
+            <div id="zoom" className='child3 flex-child'><div className="inner">C</div></div>
+            <div id="zoom" className='child4 flex-child'><div className="inner">D</div></div>
+            <div id="zoom" className='child5 flex-child'><div className="inner">E</div></div>
+            <div id="zoom" className='child6 flex-child'><div className="inner">F</div></div>
+            <div id="zoom" className='child7 flex-child'><div className="inner">G</div></div>
+            <div id="zoom" className='child8 flex-child'><div className="inner">H</div></div>
           </div>
-          </div>
-          </div>
+        <Footer />
+        <About />
+      </div>
+    </div>
 
 
     );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-    HI
+    <div className="footer">
+    FOOTER
     </div>
   )
 }
