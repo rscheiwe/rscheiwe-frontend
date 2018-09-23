f34d2cb67cc5fd502dac6de5b13fcffc5bf99cd6
rscheiwe <rscheiwe@gmail.com>
2018-09-23 10:36:54 -0400

    refactored demos

--- src/components/Demos.js
+++ src/components/Demos.js
@@ -5,23 +5,34 @@ class Demos extends Component {
 
   render() {
     return(
-      <div className="demos-main">
-        <span className="iframe-mirror">
-          <iframe width="420" height="315"
-            src="https://www.youtube.com/embed/tgbNymZ7vqY">
-          </iframe>
-        </span>
-        <span className="iframe-mirror">
-          <iframe width="420" height="315"
-            src="https://www.youtube.com/embed/tgbNymZ7vqY">
-          </iframe>
-        </span>
-        <span className="iframe-mirror">
-          <iframe width="420" height="315"
-            src="https://www.youtube.com/embed/tgbNymZ7vqY">
-          </iframe>
-        </span>
+      <div>
+        <a href="/" >
+          <img className="main-icon" src='./richard-scheiwe_icon.png' style={{width:'10%', position:'relative !important'}}/>
+        </a>
+        <div className="demos-main">
+          <span className="iframe-mirror">
+            <iframe width="100%" height="100%" frameborder="0" allowFullScreen
+              src="https://www.youtube.com/embed/tgbNymZ7vqY">
+            </iframe>
+            <hr />
+            <h4 style={{ textAlign:'right', fontFamily:'Sarpanch', letterSpacing:'4px'}}>JACOTO</h4>
+          </span>
+          <span className="iframe-mirror">
+            <iframe width="100%" height="100%"
+              src="https://www.youtube.com/embed/tgbNymZ7vqY">
+            </iframe>
+            <hr />
+            <h4 style={{ textAlign:'right', fontFamily:'Sarpanch', letterSpacing:'4px'}}>JARVIFY</h4>
+          </span>
+          <span className="iframe-mirror">
+            <iframe width="100%" height="100%"
+              src="https://www.youtube.com/embed/tgbNymZ7vqY">
+            </iframe>
+            <hr />
+            <h4 style={{ textAlign:'right', fontFamily:'Sarpanch', letterSpacing:'4px'}}>MEDIA OCEAN</h4>
+          </span>
 
+      </div>
       </div>
     )
   }
