1fbb5e15b46b8d95331d5f1663f70389213cce24
rscheiwe <rscheiwe@gmail.com>
2018-09-22 23:37:03 -0400

    finished overlay

--- src/App.js
+++ src/App.js
@@ -4,37 +4,46 @@ import Slices from './components/Slices.js'
 import Footer from './components/Footer.js'
 import About from './components/About.js'
 
-
-
-
-
+import Loader from 'react-loader-advanced';
 
 import './App.css';
 
+
+
 class App extends Component {
 
+  state = {
+    isActive:false
+  }
 
+  handleClick = () => {
+    this.setState(prevState => ({
+        isActive:!prevState.isActive
+      })
+    )
+  }
 
   render() {
-
+    const customMessageElement = (
+      <div className='about-page' onClick={this.handleClick}></div>
+    );
 
     return (
 
       <div >
 
-
-
+      <Loader show={this.state.isActive} onClick={this.handleClick} message={customMessageElement} >
 
         <Headerx />
 
         <div className="Container">
-
+        
           <Slices />
           <Footer />
-          <About />
+          <About handleClick={ this.handleClick } />
 
         </div>
-
+        </Loader>
 
       </div>
 
--- src/components/About.js
+++ src/components/About.js
@@ -3,13 +3,16 @@ import React, { Component } from 'react'
 
 class About extends Component {
 
+  handlePass = () => {
+    this.props.handleClick()
+  }
 
 
   render() {
-    
+
 
     return (
-      <div className="about grayscale">
+      <div className="about grayscale" onClick={ this.handlePass }>
         <div  className="overlay" />
           <img  className="about-img" src='/images/richard-scheiwe_question.png' />
 
