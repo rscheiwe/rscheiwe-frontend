99d048b3b3172998dacec26bf473386813a6b9a1
rscheiwe <rscheiwe@gmail.com>
2018-09-24 21:19:13 -0400

    added RSS/JSON encoding feed

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,8 @@ import Slices from './components/Slices.js'
 import Footer from './components/Footer.js'
 import About from './components/About.js'
 
+import mediumJSONFeed from 'medium-json-feed'
+
 import Loader from 'react-loader-advanced';
 
 import './App.css';
@@ -23,17 +25,24 @@ class Home extends Component {
     )
   }
 
+  handleFetch = () => {
+    mediumJSONFeed('@rscheiwe/latest')
+      .then(console.log)
+  }
+
   render() {
     const customMessageElement = (
       <div className='about-extend' onClick={this.handleClick}>
       <div className='about-page' >
         <img className="about-page-item" src='/images/richard-scheiwe_bio.png' />
+        <span>
         <h3 className="about-page-item" style={{fontFamily:'Sarpanch', letterSpacing:'4px'}}>
           RICHARD SCHEIWE
         </h3>
-        <p className="about-page-item" style={{fontFamily:'Sarpanch', textAlign:'right'}}>
+        <p className="about-page-item" style={{fontFamily:'Sarpanch', textAlign:'right', padding:'2em'}}>
         Currently, I’m exploring ways to synchronize Python-driven machine-learning with Ruby on Rails and Node.js backends. My passion for software engineering stems from taking the intensive Machine-learning and Neural Network course in Python at General Assembly in 2017. With over a decade in entrepreneurship and teaching, I have long been attracted to creating new experiences for people in which they draw value from learning something new or simply reorienting their views of the world. These professional experiences allow me to bring strong skills in critical thinking, team-building, and creative problem solving through coding.
         </p>
+        </span>
       </div>
       </div>
     );
@@ -42,10 +51,12 @@ class Home extends Component {
 
       <div >
 
+      <button onClick={this.handleFetch}> HI </button>
+
       <Loader show={this.state.isActive}
               onClick={this.handleClick}
               message={customMessageElement}
-
+              backgroundStyle={{height:'110%'}}
               contentBlur={5}
               >
 
