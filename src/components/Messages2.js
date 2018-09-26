97fb559247589c9f4e6a2d4f7a304bf97efe878d
rscheiwe <rscheiwe@gmail.com>
2018-09-26 18:31:40 -0400

    added individual slice pages

--- src/components/Messages2.js
+++ src/components/Messages2.js
@@ -3,11 +3,16 @@ import React from 'react'
 const CustomMessageElement = (props) => {
     return (
       <div className='about-extend' onClick={props.handleClick}>
+
       <div className='article-page' >
-      <span>
-        <img className="about-page-item" src='/images/jacoto_logo.png' alt="richard-scheiwe_bio" />
-        <img className="about-page-item" src='/images/jacoto_logo.png' alt="richard-scheiwe_bio" />
-      </span>
+        <span>
+          <a href="http://jacoto.herokuapp.com" target="_blank" rel="noopener noreferrer">
+            <img className="info-page-item" src='/images/jacoto_logo.png' alt="richard-scheiwe_bio"/>
+          </a>
+          <a href="http://jacoto.herokuapp.com" target="_blank" rel="noopener noreferrer">
+            <img className="info-page-item" src='/images/jacoto-homepage.png' alt="richard-scheiwe_bio" />
+          </a>
+        </span>
 
         <span>
         <h3 className="about-page-item" style={{fontFamily:'Sarpanch', letterSpacing:'4px'}}>
@@ -16,6 +21,9 @@ const CustomMessageElement = (props) => {
         <p className="about-page-item" style={{fontFamily:'Sarpanch', textAlign:'right', padding:'2em'}}>
         Currently, I’m exploring ways to synchronize Python-driven machine-learning with Ruby on Rails and Node.js backends. My passion for software engineering stems from taking the intensive Machine-learning and Neural Network course in Python at General Assembly in 2017. With over a decade in entrepreneurship and teaching, I have long been attracted to creating new experiences for people in which they draw value from learning something new or simply reorienting their views of the world. These professional experiences allow me to bring strong skills in critical thinking, team-building, and creative problem solving through coding.
         </p>
+        <a href="https://www.github.com/rscheiwe/jacoto-frontend" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-github fa-5x" style={{color:'black'}}></i>
+        </a>
         </span>
       </div>
       </div>
@@ -25,7 +33,29 @@ const CustomMessageElement = (props) => {
 const CustomMessageElement2 = (props) => {
     return (
       <div className='about-extend' onClick={props.handleClick2}>
-        <img className="loader-jarvify-logo" src='/images/jarvify_logox1.png' alt="placeholder" width='200px' height='200px'/>
+
+      <div className='article-page' >
+        <span>
+          <a href="http://jarvify.herokuapp.com" target="_blank" rel="noopener noreferrer">
+            <img className="info-page-item" src='/images/jarvify_logox1.png' alt="richard-scheiwe_bio"/>
+          </a>
+          <a href="http://jarvify.herokuapp.com" target="_blank" rel="noopener noreferrer">
+            <img className="info-page-item" src='/images/jarvify-homepage.png' alt="richard-scheiwe_bio" />
+          </a>
+        </span>
+
+        <span>
+        <h3 className="about-page-item" style={{fontFamily:'Sarpanch', letterSpacing:'4px'}}>
+          RICHARD SCHEIWE
+        </h3>
+        <p className="about-page-item" style={{fontFamily:'Sarpanch', textAlign:'right', padding:'2em'}}>
+        Currently, I’m exploring ways to synchronize Python-driven machine-learning with Ruby on Rails and Node.js backends. My passion for software engineering stems from taking the intensive Machine-learning and Neural Network course in Python at General Assembly in 2017. With over a decade in entrepreneurship and teaching, I have long been attracted to creating new experiences for people in which they draw value from learning something new or simply reorienting their views of the world.
+        </p>
+        <a href="https://www.github.com/rscheiwe/JarvCore" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-github fa-5x" style={{color:'black'}}></i>
+        </a>
+        </span>
+      </div>
       </div>
     )
   };
@@ -39,7 +69,29 @@ const CustomMessageElement3 = (props) => {
 const CustomMessageElement4 = (props) => {
     return (
       <div className='about-extend' onClick={props.handleClick4}>
-        <img className="loader-jarvify-logo" src='/images/mediaoceanlogo.png' alt="placeholder"/>
+
+      <div className='article-page' >
+        <span>
+          <a href="http://glacial-depths-45879.herokuapp.com" target="_blank" rel="noopener noreferrer">
+            <img className="info-page-item" src='/images/mediaoceanlogo.png' alt="richard-scheiwe_bio"/>
+          </a>
+          <a href="http://glacial-depths-45879.herokuapp.com" target="_blank" rel="noopener noreferrer">
+            <img className="info-page-item" src='/images/mediaocean-homepage.png' alt="richard-scheiwe_bio" />
+          </a>
+        </span>
+
+        <span>
+        <h3 className="about-page-item" style={{fontFamily:'Sarpanch', letterSpacing:'4px'}}>
+          RICHARD SCHEIWE
+        </h3>
+        <p className="about-page-item" style={{fontFamily:'Sarpanch', textAlign:'right', padding:'2em'}}>
+        Currently, I’m exploring ways to synchronize Python-driven machine-learning with Ruby on Rails and Node.js backends. My passion for software engineering stems from taking the intensive Machine-learning and Neural Network course in Python at General Assembly in 2017. With over a decade in entrepreneurship and teaching, I have long been attracted to creating new experiences for people in which they draw value from learning something new or simply reorienting their views of the world.
+        </p>
+        <a href="https://www.github.com/rscheiwe/media_ocean" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-github fa-5x" style={{color:'black'}}></i>
+        </a>
+        </span>
+      </div>
       </div>
     )
   };
