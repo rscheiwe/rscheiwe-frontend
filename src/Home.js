d09f7849f8bb823a53c40e98f09e5d0cb2cbe9cf
rscheiwe <rscheiwe@gmail.com>
2018-09-30 17:06:16 -0400

    finished responsive styling

--- src/Home.js
+++ src/Home.js
@@ -28,13 +28,13 @@ class Home extends Component {
       <div className='about-page' >
         <img className="about-page-item-pic" src='/images/richard-scheiwe_bio.png' alt="richard-scheiwe_bio"/>
         <span>
-        <h3 className="about-page-item" style={{ textAlign:'left', fontFamily:'Sarpanch', fontWeight:'300', letterSpacing:'4px', padding:'2em'}}>
+        <h3 className="about-page-item" style={{ textAlign:'left', fontFamily:'Sarpanch', fontWeight:'300', letterSpacing:'4px', padding:'1em'}}>
           RICHARD SCHEIWE
         </h3>
-        <p className="about-page-item2" style={{fontFamily:'Roboto Condensed', fontWeight:'300', textAlign:'left', padding:'2em'}}>
+        <p className="about-page-item2" style={{fontFamily:'Roboto Condensed', fontWeight:'300', textAlign:'left', padding:'1em'}}>
         Currently, I’m exploring ways to synchronize Python-driven machine-learning through Node.js. My passion for software engineering stems from taking the intensive Machine-learning and Neural Network course in Python at General Assembly in 2017, followed by Flatiron's 15-week Immersive Software Engineering program. With over a decade in entrepreneurship and teaching, I have long been attracted to creating new experiences for people in which they draw value from learning something new or simply reorienting their views of the world.
         </p>
-        <p className="about-page-item2" style={{fontFamily:'Roboto Condensed', fontWeight:'300', textAlign:'left', padding:'2em'}}>
+        <p className="about-page-item2" style={{fontFamily:'Roboto Condensed', fontWeight:'300', textAlign:'left', padding:'1em'}}>
         Skills: Python, Javascript, Ruby, React/Redux, Ethereum/Solidity, PostgreSQL, Pandas, Django, Selenium, AWS Lambda, Heroku, HTML, CSS
         </p>
         </span>
@@ -45,9 +45,7 @@ class Home extends Component {
     return (
 
       <div >
-
-
-
+      
       <Loader show={this.state.isActive}
               onClick={this.handleClick}
               message={customMessageElement}
